3cbee8d8fd12994a7d141e3b65385afeb743e679
AdrianMarin1494 <marin.adrian1494@gmail.com>
2023-06-05 19:12:57 +0300

    update for routing

--- react_router_second/react_router/src/pages/AllQuotes.js
+++ react_router_second/react_router/src/pages/AllQuotes.js
@@ -1,6 +1,13 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+    {id: "q1", author: "Adrian", text: "Learning react"},
+    {id: "q2", author: "Marin", text: "Learning angular"},
+]
+
 const AllQuotes = () => {
     return (
-        <h1>All Quotes</h1>
+        <QuoteList quotes={DUMMY_QUOTES}/>
     );
 };
 
--- react_router_second/react_router/src/pages/NewQuote.js
+++ react_router_second/react_router/src/pages/NewQuote.js
@@ -1,6 +1,17 @@
+import { useHistory } from "react-router-dom";
+
+import QuoteForm from "../components/quotes/QuoteForm";
+
 const NewQuote = () => {
+    const history = useHistory();
+    
+    const addQuoteHandler = quoteData => {
+        console.log(quoteData);
+
+        history.push(quoteData);
+    };
     return (
-        <h1>New Quote Page</h1>
+        <QuoteForm onAddQuote={addQuoteHandler} />
     );
 };
 
